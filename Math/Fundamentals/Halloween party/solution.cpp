e50887b06935de6c7074b8947621363187187325
tai man chan <46643763+chantaiman1998@users.noreply.github.com>
2021-08-21 01:57:40 +0800

    Create solution.cpp

---
+++ Math/Fundamentals/Halloween party/solution.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'halloweenParty' function below.
+ *
+ * The function is expected to return a LONG_INTEGER.
+ * The function accepts INTEGER k as parameter.
+
+long halloweenParty(int k) {
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string t_temp;
+    getline(cin, t_temp);
+
+    int t = stoi(ltrim(rtrim(t_temp)));
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        string k_temp;
+        getline(cin, k_temp);
+
+        int k = stoi(ltrim(rtrim(k_temp)));
+
+        long result = halloweenParty(k);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+ */
+ 
+ int main() {
+     int T;
+     long int K, K2, result;
+     cin >> T;
+     for(int i = 0; i < T; i++){
+         cin >> K;
+         K2 = K / 2;
+         if(K2 * 2 == K)
+            result = K2 * K2;
+         else
+            result = K2 * (K2+1);
+        cout << result << endl;
+     }
+     return 0;
+ }
+ 
+ 
+ 
